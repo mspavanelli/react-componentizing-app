36c855777463c3431eb8bb1c8a04ce76934ef2cc
Matheus Pavanelli <mspavanelli@gmail.com>
2021-04-07 00:00:23 -0300

    apply prettier

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,26 +1,23 @@
-import { Star, Clock } from 'react-feather';
+import { Star, Clock } from 'react-feather'
 
-import '../styles/movie-card.scss';
+import '../styles/movie-card.scss'
 
 interface MovieCardProps {
-  title: string;
-  poster: string;
-  rating: string;
-  runtime: string;
+  title: string
+  poster: string
+  rating: string
+  runtime: string
 }
 
 export function MovieCard(props: MovieCardProps) {
   return (
-    <div className="movie-card">
-      <img
-        src={props.poster}
-        alt={props.title}
-      />
+    <div className='movie-card'>
+      <img src={props.poster} alt={props.title} />
 
       <div>
-        <div className="movie-info">
+        <div className='movie-info'>
           <span>{props.title}</span>
-          <div className="meta">
+          <div className='meta'>
             <div>
               <Star /> {props.rating}
             </div>
